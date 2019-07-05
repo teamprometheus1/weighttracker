5f1748d5f096494ddf9532a063dbef3348e2cf7c
CassioP <cpeluso@google.com>
2019-07-05 15:33:13 +0100

    arrumado o insert do sqlite

--- weighttracker.py
+++ weighttracker.py
@@ -1,12 +1,25 @@
 import sqlite3
+from datetime import date 
+
+peso = int(input("Qual o peso de hoje? \n\n"))
+
+dia = date.today().day
+
+mes = date.today().month
+
+ano = date.today().year
+
+data_pesagem = str("{} - {} - {}".format(dia, mes, ano))
 
 conn = sqlite3.connect("peso.db")
 c = conn.cursor()
-c.execute("""CREATE TABLE (
-            first text
+c.execute("""CREATE TABLE peso (
+            first text,
             second real
     )""")
+     
+c.execute("INSERT INTO peso Values (?,?)",(data_pesagem,peso))
+
 conn.commit()
+
 conn.close()
-
-
